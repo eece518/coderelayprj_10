f67804165763fd4fc433dac5026a6bc50456ff5f
haijunq <haijunq@gmail.com>
2014-04-24 22:44:17 -0700

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script language="JavaScript">
+VIH_BackColor = "palegreen";
+VIH_ForeColor = "navy";
+VIH_FontPix = "16";
+VIH_DisplayFormat = "You are visiting from:<br>IP Address: %%IP%%<br>Host: %%HOST%%";
+VIH_DisplayOnPage = "yes";
+</script>
+<script language="JavaScript" src="http://scripts.hashemian.com/js/visitorIPHOST.js.php"></script>
+
+</body>
+</html>
